8ea7bee1f0a7d53ee956feffb472d4ea024b7aa9
Florian <florian.leman@gmail.com>
2015-11-13 09:56:13 +0100

    initial

---
+++ com.mia.formation.generation.ui/src/com/mia/formation/generation/ui/OperationView.java
@@ -0,0 +1,33 @@
+package com.mia.formation.generation.ui;
+
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.SWT;
+
+public class OperationView extends Shell {
+
+
+	
+	public void quitter(){
+// Start of user code for quitter
+// End of user code
+	}
+
+	/**
+	 * Create the shell.
+	 * @param display
+	 */
+	public OperationView(Display display) {
+		super(display, SWT.SHELL_TRIM);
+		createContents();
+	}
+	
+	/**
+	 * Create contents of the shell.
+	 */
+	protected void createContents() {
+		setText("Vue des opérations");
+		setSize(450, 300);
+	}
+}
